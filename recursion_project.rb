477fa8b6a88769e1cb99e01b78a303f2a0f335a2
Rohan James <rohanjames9282@gmail.com>
2022-09-07 11:56:03 +0530

    Finish fib without recursion

--- recursion_project.rb
+++ recursion_project.rb
@@ -6,6 +6,20 @@ require 'pry-byebug'
 
 
 #Fibonacci Sequence without recursion
-
+# This method takes a number(n) and returns an array
+# containing n amount of fibonacci numbers.
 def fib(n)
+  output = []
+  i = 0
+  while i < n
+    case i
+    when 0 then output << 0
+    when 1 then output << 1
+    else output << output[i - 1] + output[i - 2]
+    end
+    i += 1
+  end
+  output
 end
+
+p fib(8)
