68f7bfbc946d9e06b1cc92d0a41f76e682601ec8
Rohan James <rohanjames9282@gmail.com>
2022-09-08 14:49:32 +0530

    Add basecase and recursive logic

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,5 +2,12 @@ require 'pry-byebug'
 
 def merge_sort(arr)
 
+return arr if arr.length == 1
+
+  index_1 = arr.length/2.round
+  index_2 = arr.length - index_1
+
+  left_half = merge(arr[0..index_1])
+  right_half = merge(arr[index_1..index_2])
 end
 
