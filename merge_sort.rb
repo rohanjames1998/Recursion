fd9b5f8d31fd70d7bd57a9b8c233bcfe302896b0
Rohan James <rohanjames9282@gmail.com>
2022-09-09 14:33:40 +0530

    Change conditionals to delete the item they push in output

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,11 +1,8 @@
 require 'pry-byebug'
 
 def merge_sort(arr)
-  if arr.length == 2
-    left_half = Array.new
-    left_half << arr[0]
-    right_half = Array.new
-    right_half << arr[1]
+  if arr.length == 1
+   return arr
   else
     index_1 = arr.length/2.round
     index_2 = arr.length
@@ -13,24 +10,43 @@ def merge_sort(arr)
     right_half = merge_sort(arr[index_1...index_2])
   end
   output = []
-right_half.each_with_index do |ele, i|
-  if left_half[i] == nil
-    output << ele
-  elsif ele > left_half[i]
-    output << left_half[i]
-    output << ele
-  elsif ele < left_half[i]
-    output << ele
-    output << left_half[i]
-  else
-    output << ele
-    output << left_half[i]
-  end
-  return output
+  loop do
+    if right_half[0] < left_half[0]
+      output << right_half[0]
+      right_half.delete_at(0)
+    elsif left_half[0] > right_half[0]
+      output << left_half[0]
+      left_half.delete_at(0)
+    else
+      output << right_half[0]
+      right_half.delete_at(0)
+    end
+    if right_half.empty?
+      output << left_half
+      output.flatten!
+    end
+    break if right_half.empty?
 end
+return output
 end
 
 
 
-p merge_sort([4,3,2,1])
+p merge_sort([4,3,2,1,0])
 
+
+# loop do
+# if left_half[i] == nil
+#   output << ele
+# elsif ele > left_half[i]
+#   output << left_half[i]
+#   left_half.delete_at(i)
+# elsif ele < left_half[i]
+#   output << ele
+#   right_half.delete_at(i)
+# else
+#   output << ele
+#   output << left_half[i]
+# end
+# i += 1
+# break if right_half
