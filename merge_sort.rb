a22317b2562b9631f78f66991855f5eb52b3f5eb
Rohan James <rohanjames9282@gmail.com>
2022-09-09 15:00:42 +0530

    Remove useless conditional statements

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,40 +1,39 @@
 require 'pry-byebug'
 
 def merge_sort(arr)
+  # Base Case: Return the sorted (1 length) array.
+  return arr if arr.length == 1
+  # Dividing array into two parts as evenly as possible.
+    index = arr.length/2.round
+    left_half = merge_sort(arr[0...index])
+    right_half = merge_sort(arr[index...arr.length])
 
-  if arr.length == 1
-   return arr
-  else
-    index_1 = arr.length/2.round
-    index_2 = arr.length
-    left_half = merge_sort(arr[0...index_1])
-    right_half = merge_sort(arr[index_1...index_2])
-  end
   output = []
   loop do
-    if right_half[0] < left_half[0]
+    # Comparing and deleting first elements to avoid duplications.
+    if right_half[0] <= left_half[0]
       output << right_half[0]
       right_half.delete_at(0)
-    elsif left_half[0] < right_half[0]
+    elsif left_half[0] <= right_half[0]
       output << left_half[0]
       left_half.delete_at(0)
-    else
-      output << right_half[0]
-      right_half.delete_at(0)
     end
+
+    # If one half becomes empty, we just add the other half to output and break the loop.
     if right_half.empty?
       output << left_half
       output.flatten!
+      break
     elsif left_half.empty?
       output << right_half
       output.flatten!
+      break
     end
-    break if right_half.empty? || left_half.empty?
 end
 return output
 end
 
 
 
-p merge_sort([5,4,3,2,1])
+p merge_sort([12,3,0,4121,31,44,2121,31,0])
 
